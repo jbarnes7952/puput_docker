23e7f576d737353cab00f5e5b441210456c9e6c1
jbarnes <jbarnes7952@gmail.com>
2018-01-11 09:42:14 -0500

    working

--- puput-patch/load_data.sh
+++ puput-patch/load_data.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-expect my_migrate.exp
-./manage.py migrate --run-syncdb
-./manage.py puput_initial_data
-./manage.py createsuperuser
+expect my_migrate.exp && ./manage.py migrate --run-syncdb && ./manage.py loaddata blog_init.json
+# ./manage.py puput_initial_data
+# ./manage.py createsuperuser
